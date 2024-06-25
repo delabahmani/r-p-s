943c468b10d6cfa3292c27060b853614a32b016d
delabahmani <delabahmani@gmail.com>
2024-06-25 15:53:22 -0400

    implement buttons

--- src/app/components/Rules.tsx
+++ src/app/components/Rules.tsx
@@ -15,11 +15,13 @@ export default function Rules({ advanced, setOpen, open }: RulesProps) {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-0 z-50 h-full w-full bg-white p-5 lg:left-1/2 lg:top-1/2 lg:h-[60%] lg:w-[30%] lg:-translate-x-1/2 lg:-translate-y-1/2 lg:transform lg:p-[30px] md:rounded-lg"
+          className="fixed inset-0 z-50 h-full w-full bg-white p-5 md:rounded-lg lg:left-1/2 lg:top-1/2 lg:h-[60%] lg:w-[30%] lg:-translate-x-1/2 lg:-translate-y-1/2 lg:transform lg:p-[30px]"
           onClick={(e) => e.stopPropagation()}
         >
           <div className="relative flex flex-col items-center justify-between uppercase lg:flex-row lg:px-7">
-            <h1 className="text-dark text-center text-4xl font-bold mt-10 lg:mt-0">Rules</h1>
+            <h1 className="relative top-8 mt-10 text-center text-3xl font-bold text-dark lg:mt-0 lg:text-4xl">
+              Rules
+            </h1>
             <img
               src="/icon-close.svg"
               alt="close button"
@@ -28,27 +30,31 @@ export default function Rules({ advanced, setOpen, open }: RulesProps) {
             />
           </div>
 
-          <div className="flex flex-col items-center justify-center w-full mt-4 lg:mt-0 h-[70vh] lg:h-full">
-            <div className="flex items-center justify-center ">
-            {advanced ? (
-              <img
-                src="/image-rules-bonus.svg"
-                alt="rules for advanced"
-                className="mx-auto"
-              />
-            ) : (
-              <img src="/image-rules.svg" alt="rules for normal" className="mx-auto" />
-            )}
+          <div className="mt-4 flex h-[70vh] w-full flex-col items-center justify-center lg:mt-0 lg:h-full">
+            <div className="flex items-center justify-center">
+              {advanced ? (
+                <img
+                  src="/image-rules-bonus.svg"
+                  alt="rules for advanced mode"
+                  className="mx-auto"
+                />
+              ) : (
+                <img
+                  src="/image-rules.svg"
+                  alt="rules for normal mode"
+                  className="mx-auto"
+                />
+              )}
             </div>
           </div>
 
-          <div className="flex justify-center mt-4 lg:hidden">
-          <img
-            src="/icon-close.svg"
-            alt="close button"
-            className="h-6 w-6 cursor-pointer"
-            onClick={() => setOpen(false)}
-          />
+          <div className="mt-4 flex justify-center lg:hidden">
+            <img
+              src="/icon-close.svg"
+              alt="close button"
+              className="h-5 w-5 cursor-pointer lg:h-6 lg:w-6"
+              onClick={() => setOpen(false)}
+            />
           </div>
         </motion.div>
       )}
